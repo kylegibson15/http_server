ada7dfeebe4936a6e14ca52f6cbef3ce6fbe7413
kylegibson15 <kylegibson15@gmail.com>
2021-01-03 23:24:12 -0700

    Statuses

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use server::Server;
-use http::Request;
-use http::Method;
+// use http::Request;
+// use http::Method;
 
 mod http;
 mod server;
 
 fn main() {
-    let address_and_port = String::from("127.0.0.1:8080").to_string();
-    let server = Server::new(address_and_port);
+    let server = Server::new(String::from("127.0.0.1:8080").to_string());
     server.run(); 
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,8 @@
+use std::net::TcpListener;
+use std::io::{Read, Write};
+use crate::http::{Request};
+use std::convert::{TryInto, TryFrom};
+
 pub struct Server {
     addr: String,
 }
@@ -8,6 +13,34 @@ impl Server {
     }
 
     pub fn run(self) {
-        print!("Listening on {}...", self.addr);
+        print!("Application Running\n");
+        print!("Listening on {}...\n", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                    // let response = 
+                                    write!(stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+                                }
+                                Err(e) => print!("Failed to parse a request: {}", e),
+                            }
+                            // let res: &Result<Request, _> = &buffer[..].try_into();
+                        }
+                        Err(e) => print!("Failed to read from connection: {}", e)
+                    }
+                }
+                Err(e) => print!("Failed to establish a connection: {}", e)
+            }
+        }
     }
 }
